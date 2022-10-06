fb36e2f4bc22472d41b252ab73f504bf019cf828
Nguyễn Song Toàn <nguyensongtoan0302@gmail.com>
2022-10-07 00:25:48 +0700

    fix cors

--- api/src/main/java/org/aibles/okrs/api/controller/OkrsController.java
+++ api/src/main/java/org/aibles/okrs/api/controller/OkrsController.java
@@ -6,12 +6,14 @@ import org.aibles.okrs.core_business.dto.request.CreateOkrsRequest;
 import org.aibles.okrs.core_business.dto.response.ObjectiveResponsesDTO;
 import org.aibles.okrs.core_business.service.OkrsService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/okrs")
